6a4a9cb9b23616d3f1a65ed03c1cc4e668dd1ad9
zcwwzdjn <zcwwzdjn@hotmail.com>
2014-05-11 22:53:11 +0800

    modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Role.create(name: 'User')
+Role.create(name: 'ContestAdmin')
+Role.create(name: 'SuperAdmin')
+
+Account.create(email: 'admin@example.com', password: 'password', role: Role.where(name: 'SuperAdmin').first)
